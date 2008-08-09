ff4c5c02ee9fea489f7a37a6d4bd9e8475f76cc3
Joachim Breitner <mail@joachim-breitner.de>
2008-08-09 13:48:15 -0300

    setup.py: Only compile theme on "build"
    To ease packaging and general user experience, this patch makes sure
    that the file ./data/themes/zhone.edj is only generated when doing
    "build" and removed on "clean".

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+from distutils.command.build import build as _build
+from distutils.command.clean import clean as _clean
+import os
 
-# compile theme files
-import subprocess
-result = subprocess.call( "cd ./data/themes; edje_cc -v -id ../images -fd ../fonts zhone.edc", shell=True )
-if result != 0:
-    raise Exception( "Can't build theme files. Built edje_cc?" )
+class my_build(_build):
+    def run(self):
+        _build.run(self)
+
+        # compile theme files
+        import subprocess
+        result = subprocess.call( "cd ./data/themes; edje_cc -v -id ../images -fd ../fonts zhone.edc", shell=True )
+        if result != 0:
+            raise Exception( "Can't build theme files. Built edje_cc?" )
+
+class my_clean(_clean):
+    def run(self):
+        _clean.run(self)
+
+        if os.path.exists('./data/themes/zhone.edj'):
+            os.remove('./data/themes/zhone.edj')
 
 setup(
     name = "zhone",
@@ -17,7 +31,9 @@ setup(
     ext_modules = [
         Extension( "illume", ["src/illume.pyx"], libraries = ["ecore", "ecore_x"] )
         ],
-    cmdclass = { 'build_ext': build_ext },
+    cmdclass = { 'build_ext': build_ext ,
+                 'build'    : my_build  ,
+                 'clean'    : my_clean  },
     scripts = [ "src/zhone" ],
     data_files = [
         ( "zhone/", ["data/themes/zhone.edj"] ),
