2638381cf581d167cf5304baf64ae678016428a6
Martin Jansa <Martin.Jansa@gmail.com>
2010-02-09 11:26:03 +0100

    setup.py: use pkgconfig call for checking eina-0 include headers (as used in python-ecore)

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ from Cython.Distutils import build_ext
 from distutils.command.build import build as _build
 from distutils.command.clean import clean as _clean
 import os
+import subprocess
+import shlex
 
 class my_build(_build):
     def run(self):
@@ -22,6 +24,38 @@ class my_clean(_clean):
         if os.path.exists('./data/themes/zhone.edj'):
             os.remove('./data/themes/zhone.edj')
 
+def getstatusoutput(cmdline):
+    cmd = shlex.split(cmdline)
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    out, err = p.communicate()
+    return p.returncode, out
+
+def pkgconfig(*packages, **kw):
+    flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries',
+                '-D': 'prepro_vars'}
+    pkgs = ' '.join(packages)
+    cmdline = 'pkg-config --libs --cflags %s' % pkgs
+
+    status, output = getstatusoutput(cmdline)
+    if status != 0:
+        raise ValueError("could not find pkg-config module: %s" % pkgs)
+
+    for token in output.split():
+        flag  = flag_map.get(token[:2], None)
+        if flag is not None:
+            kw.setdefault(flag, []).append(token[2:])
+        elif token.startswith("-Wl,"):
+            kw.setdefault("extra_link_args", []).append(token)
+        else:
+            kw.setdefault("extra_compile_args", []).append(token)
+
+    if "extra_link_args" in kw:
+        print "Using extra_link_args: %s" % " ".join(kw["extra_link_args"])
+    if "extra_compile_args" in kw:
+        print "Using extra_compile_args: %s" % " ".join(kw["extra_compile_args"])
+
+    return kw
+
 setup(
     name = "zhone",
     version = "milestone4.1+git",
@@ -29,7 +63,7 @@ setup(
     author_email = "mlauer@vanille-media.de",
     url = "http://www.freesmartphone.org",
     ext_modules = [
-        Extension( "illume", ["src/illume.pyx"], libraries = ["ecore", "ecore_x"] )
+        Extension( "illume", sources=['src/illume.pyx'], **pkgconfig('"ecore >= 0.9.9.050" ''"eina-0 >= 0.0.1"') )
         ],
     cmdclass = { 'build_ext': build_ext ,
                  'build'    : my_build  ,
